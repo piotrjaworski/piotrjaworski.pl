46ac1149913458895b493b37608ee6fab6b98980
Piotr Jaworski <piotrek.jaw@gmail.com>
2017-08-24 00:05:11 +0200

    App update

--- app/app.rb
+++ app/app.rb
@@ -10,6 +10,61 @@ set :public_dir, 'public'
 get '/' do
   @tutorials = [
     {
+      title: "Hanami – Ruby Web Framework - Review",
+      description: "A review which covers and briefly describes Hanami - Ruby web framework.",
+      link: "https://www.nopio.com/blog/hanami-ruby-web-framework-review"
+    },
+    {
+      title: "Ruby State Machine – AASM Tutorial with Sequel, SQLite, Rake and RSpec",
+      description: "A tutorial which shows features of AASM - Ruby state machine connected with Sequel, SQLite and Rake.",
+      link: "https://www.nopio.com/blog/ruby-state-machine-aasm-tutorial"
+    },
+    {
+      title: "Rails API with Active Model Serializers – Part 2",
+      description: "The second part of the tutorial which covers how to create a secure Rails API using HTTP Tokens and ActiveModelSerializers.",
+      link: "https://www.nopio.com/blog/rails-api-tests-rspec"
+    },
+    {
+      title: "Rails API with Active Model Serializers – Part 1",
+      description: "The first part of the tutorial which covers how to create a secure Rails API using HTTP Tokens and ActiveModelSerializers.",
+      link: "https://www.nopio.com/blog/rails-api-active-model-serializers"
+    },
+    {
+      title: "Web Scraping in Ruby with Watir",
+      description: "An article which covers how to automatically parse rich web pages using Watir.",
+      link: "https://www.nopio.com/blog/web-scraping-with-watir"
+    },
+    {
+      title: "How to set up Local Environment for Rails with SublimeText",
+      description: "Article which covers local development configuration for Rails and JavaScript using SublimeText on macOS / UNIX.",
+      link: "https://www.nopio.com/blog/local-environment-configuration-rails-sublimetext"
+    },
+    {
+      title: "Rails Chat Application – part 2",
+      description: "Tutorial which covers simple chat applications using Action Cable - new Rails 5 feature.",
+      link: "https://www.nopio.com/blog/rails-chat-application-actioncable"
+    },
+    {
+      title: "Rails Chat Application – part 1",
+      description: "Tutorial which covers simple chat applications using Action Cable - new Rails 5 feature.",
+      link: "https://www.nopio.com/blog/rails-real-time-chat-application-part-1"
+    },
+    {
+      title: "AngularJS and Ruby on Rails Tutorial",
+      description: "Tutorial which shows how to integrate Ruby on Rails with AngularJS.",
+      link: "https://www.nopio.com/blog/angularjs-tutorial"
+    },
+    {
+      title: "React On Rails – part 3",
+      description: "Third part of the tutorial, which shows how to connect Ruby on Rails with Facebook's front-end framework - React.Js.",
+      link: "https://www.nopio.com/blog/react-pagination-rails-tutorial"
+    },
+    {
+      title: "React On Rails – part 2",
+      description: "Second part of the tutorial, which shows how to connect Ruby on Rails with Facebook's front-end framework - React.Js.",
+      link: "https://www.nopio.com/blog/react-on-rails-tutorial-part-2"
+    },
+    {
       title: "React On Rails – part 1",
       description: "First part of the tutorial, which shows how to connect Ruby on Rails with Facebook's front-end framework - React.Js.",
       link: "https://www.nopio.com/blog/react-rails-part-1-tutorial/"
