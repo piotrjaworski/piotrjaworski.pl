908b48b3623cd20e2a0dadc772eef0e43c2f2fad
Piotr Jaworski <piotrek.jaw@gmail.com>
2019-02-08 20:58:09 +0000

    Added slides

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,7 @@ get '/' do
   yaml_data = YAML.load_file('app/data.yml')
 
   @tutorials = yaml_data['tutorials']
+  @talks = yaml_data['talks']
   @opensource = yaml_data['opensource']
   @backend =  yaml_data['backend']
   @frontend = yaml_data['frontend']
@@ -30,5 +31,9 @@ helpers do
 
   def stylesheet_tag(name)
     "<link href='assets/styles/#{name}' rel='stylesheet' type='text/css' />"
+  end
+
+  def pdf_tag(pdf_name, description)
+    "<a href='/files/#{pdf_name}' target='_blank'>#{description}</a>"
   end
 end
